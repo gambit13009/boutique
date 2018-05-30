ecce185d62cfdec05f1e7c51efcac31358ea1fa8
sam13008 <ikherbane.samir@gmail.com>
2018-05-30 14:18:30 +0200

    mailjet

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 
   def welcome_email()
-     mail(from: "marcopolodu13@caramail.fr", to: "juliette8newton@gmail.com",
+     mail(from: "marcopolodu13@caramail.fr", to: "jerome.david@cegetel.net",
           subject: "This is a nice welcome email")
    end
 
