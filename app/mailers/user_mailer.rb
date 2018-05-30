24c3525b5c143aaee8b1f6621c0866ee372586e5
sam13008 <ikherbane.samir@gmail.com>
2018-05-30 19:28:35 +0200

    pls

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,10 @@ class UserMailer < ApplicationMailer
 
   def welcome_email()
 
-     mail(from: "ajerome.david@laposte.net", to: "jerome.david@cegetel.net",
+     mail(from: "ajerome.david@laposte.net", to: "jerome.david@cegetel.net", 
           subject: "Recapitulatif de votre commande")
    end
+
 
    
    
