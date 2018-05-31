04300c438b946c74816f3a59255ed5f25f0fcdf1
Jerome David <jerome.david@cegetel.net>
2018-05-31 22:12:16 +0200

    chat

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,138 +9,132 @@ def picture
 
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat grimace',
+	price: '20',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579982-6.jpg")
 
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'les mains en l'air',
+	price: '25',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579982-100-chats.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'stp',
+	price: '29',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579982-1029691327.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat à tête bizarre',
+	price: '19',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579982-boule-poils-chat.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
+	title: 'chat',
+	description: 'chat repos',
 	price: '14',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579982-chat.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'bébé chat',
+	price: '18',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579982-chat131.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat ewok',
+	price: '30',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579983-chat-moche.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
+	title: 'chat',
+	description: 'petit chat',
 	price: '14',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579983-chats-en-location.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat noeud papillon',
+	price: '16',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579983-chats-nouveaux1.jpg")
 
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat pain',
+	price: '17',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579983-chat-tete-pain.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat yeux bleus',
+	price: '21',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579983-cover.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat grognon',
+	price: '22',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527579983-le-chat-persan.jpg")
 
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat siamois',
+	price: '20',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580229-siamois-2.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'gros chat méchant',
+	price: '27',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580276-manul-chat-de-pallas-sauvage-asie-13.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat sphinx',
+	price: '15',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580352-le-sphinx.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
+	title: 'chat',
+	description: 'chat tire la langue',
 	price: '14',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580376-les-coups-de-folie-les-plus-frequents-de-nos-chats.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'gros chat',
+	price: '13',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580400-les-chats-sont-de-petits-lions.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
-	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580472-le-chat-persan.jpg")
-
-picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat pas content',
+	price: '19',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580502-detester-les-chats.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat carton',
+	price: '12',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580527-demenagement-chat.jpg")
 
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
-	price: '14',
+	title: 'chat',
+	description: 'chat moustache',
+	price: '15',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580553-cover.jpg")
 
 picture = Item.create!(
-	title: 'cat',
-	description: 'real cat',
+	title: 'chat',
+	description: 'chat crocs',
 	price: '14',
 	image_url: "https://image.noelshack.com/fichiers/2018/22/2/1527580588-comment-eloigner-les-chats-de-mon-appartement.jpg")
 
